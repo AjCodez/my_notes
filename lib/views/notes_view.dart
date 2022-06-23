21c727e061061c117f764f6baa3a862935c3eaf3
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-06-24 02:03:36 +0530

    added logout option

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,11 +9,30 @@ class NotesView extends StatefulWidget {
   State<NotesView> createState() => _NotesViewState();
 }
 
+enum MenuAction { logout }
+
 class _NotesViewState extends State<NotesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Notes")),
+      appBar: AppBar(
+        title: const Text("Notes"),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text("Log out"),
+                )
+              ];
+            },
+            onSelected: (value) {
+              print("Clicked");
+            },
+          )
+        ],
+      ),
       body: const Text("Hello Notes"),
     );
   }
