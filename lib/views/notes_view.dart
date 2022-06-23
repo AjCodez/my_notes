58912ad8c3e704c0d75b955b06325bb31a950d78
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-06-24 03:21:52 +0530

    logout completed

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -27,8 +28,12 @@ class _NotesViewState extends State<NotesView> {
                 )
               ];
             },
-            onSelected: (value) {
-              print("Clicked");
+            onSelected: (value) async {
+              if (await showLogOutDialog(context)) {
+                FirebaseAuth.instance.signOut();
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login/', (_) => false);
+              }
             },
           )
         ],
@@ -36,4 +41,27 @@ class _NotesViewState extends State<NotesView> {
       body: const Text("Hello Notes"),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Log Out"),
+          content: const Text("Are you sure you want to log out?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("No")),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Yes")),
+          ],
+        );
+      }).then((value) => value ?? false);
 }
