99a24cb69dc1e6769fb57cba12d953f6a888c241
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-03 03:27:03 +0530

    notes view

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_notes/firebase_options.dart';
 import 'package:my_notes/views/login_view.dart';
-import 'package:my_notes/views/notes_view.dart';
+import 'package:my_notes/views/notes/notes_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
