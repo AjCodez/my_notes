ce2b8f97e1081db2e672ada59add0c8ad50fcc71
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-05 18:03:59 +0530

    database

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_notes/services/crud/notes_services.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({Key? key}) : super(key: key);
@@ -8,6 +10,21 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  DatabaseNotes? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  Future<DatabaseNotes> createNote() async {
+    final existingNotes = _note;
+    if (existingNotes != null) {
+      return existingNotes;
+    }
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
